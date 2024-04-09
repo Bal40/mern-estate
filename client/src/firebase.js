58e43c089f1143665e17beb802f27918c9ea7ba7
Bal40 <102305523+Bal40@users.noreply.github.com>
2024-04-09 14:03:21 +0530

    updating Oauth Functionality

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,16 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-estate.firebaseapp.com',
-  projectId: 'mern-estate',
-  storageBucket: 'mern-estate.appspot.com',
-  messagingSenderId: '1078482850952',
-  appId: '1:1078482850952:web:28f19139ab77246602fb3d',
+  authDomain: "mern-estate-6a289.firebaseapp.com",
+  projectId: "mern-estate-6a289",
+  storageBucket: "mern-estate-6a289.appspot.com",
+  messagingSenderId: "876535292519",
+  appId: "1:876535292519:web:b7841d94b116b3521d2ac2"
 };
 
 // Initialize Firebase
